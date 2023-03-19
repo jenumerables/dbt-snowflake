2f14072fbb2244de9247522d2ff157b95fc73d52
jenumerables <jeff.rables@gmail.com>
2023-03-19 13:50:21 -0700

    Add macros, tests, and packages

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -7,8 +7,11 @@ on_schema_change='fail'
 WITH src_reviews AS (
 SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+* 
+FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
 AND date > (select max(date) from {{ this }})
 {% endif %}
+
